77524381a096478b23ff63b9d0566c297c206097
MALLESSI SAYF <saifmallessi45@gmail.com>
2023-07-03 23:57:06 +0100

    challenge-js

---
+++ Week1/Day 5/Practice/algo/challenge-js/script.js
@@ -0,0 +1,34 @@
+//1 one
+for (let i = 1; i < 21; i++) {
+    if (i % 2 != 0) {
+      console.log(i);
+    }
+  }
+  //2 one
+  for (let y = 100; y >= 0; y--) {
+    if (y % 3 == 0) {
+      console.log(y);
+    }
+  }
+  //3 one
+  
+  for (var a = 5.5; a > -4; a--) {
+    a = a - 1.5;
+    console.log(a);
+  }
+  //4 one
+  
+  var sum = 0;
+  for (let j = 1; j < 101; j++) {
+    sum += j;
+    sum + j;
+    console.log(sum);
+  }
+  
+  //5 one
+  var sum = 1;
+  for (var m = 1; m < 13; m++) {
+    sum *= m;
+    sum + m;
+    console.log(sum);
+  }
