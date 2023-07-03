572d4865aedf6e7118973b566456807651499f56
MALLESSI SAYF <saifmallessi45@gmail.com>
2023-07-04 00:05:53 +0100

    butto-js

---
+++ Week2/Day 1/Core/button-js/scripte.js
@@ -0,0 +1,11 @@
+function signout(element) {
+    console.log((element.innerText = "sign Out"));
+  }
+  
+  function custom() {
+    console.log(alert);
+  }
+  
+  function hide(element) {
+    element.remove();
+  }
